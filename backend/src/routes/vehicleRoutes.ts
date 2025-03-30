5be42d7c48a58312fd1b72be9b9b971cf823fb5a
losadev <losadauresdev@gmail.com>
2025-03-30 17:13:04 +0200

    Feature: validations for post and put created

--- backend/src/routes/vehicleRoutes.ts
+++ backend/src/routes/vehicleRoutes.ts
@@ -1,21 +1,53 @@
-import { Router, Request, Response } from "express";
-import { IVehicle } from "../types/vehicleTypes";
-import Vehicle from "../models/vehicle.model";
-import mongoose from "mongoose";
+import { Router } from "express";
 import {
   createVehicle,
   deleteVehicle,
   getVehicles,
   updateVehicle,
 } from "../controllers/vehicle.controller";
+import { body } from "express-validator";
 
 const router = Router();
 
 router.get("/", getVehicles);
 
-router.post("/", createVehicle);
+router.post(
+  "/",
+  body("make").notEmpty().withMessage("Make is required"),
+  body("model").notEmpty().withMessage("Model is required"),
+  body("year")
+    .isInt({ min: 1900, max: new Date().getFullYear() })
+    .withMessage("Year must be a valid number between 1900 and current year"),
+  body("price")
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a positive number"),
+  body("mileage")
+    .isInt({ min: 0 })
+    .withMessage("Mileage must be a positive integer"),
+  body("fuelType").notEmpty().withMessage("Fuel type is required"),
+  body("condition").notEmpty().withMessage("Condition is required"),
+  body("transmission").notEmpty().withMessage("Transmission is required"),
+  createVehicle
+);
 
-router.put("/:id", updateVehicle);
+router.put(
+  "/:id",
+  body("make").notEmpty().withMessage("Make is required"),
+  body("model").notEmpty().withMessage("Model is required"),
+  body("year")
+    .isInt({ min: 1900, max: new Date().getFullYear() })
+    .withMessage("Year must be a valid number between 1900 and current year"),
+  body("price")
+    .isFloat({ min: 0 })
+    .withMessage("Price must be a positive number"),
+  body("mileage")
+    .isInt({ min: 0 })
+    .withMessage("Mileage must be a positive integer"),
+  body("fuelType").notEmpty().withMessage("Fuel type is required"),
+  body("condition").notEmpty().withMessage("Condition is required"),
+  body("transmission").notEmpty().withMessage("Transmission is required"),
+  updateVehicle
+);
 
 router.delete("/:id", deleteVehicle);
 
