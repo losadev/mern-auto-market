4d7db90ae3de5edb70c8fd54589c4ee8e02d8ed9
losadev <losadauresdev@gmail.com>
2025-03-30 18:00:01 +0200

    operator changed on index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.use("/api/vehicles", vehicleRouter);
 
